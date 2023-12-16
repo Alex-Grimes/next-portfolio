2c8e083889444c51f50e69c28068dc874ee48414
alex <alexgrimes.se@gmail.com>
2023-12-16 17:50:59 -0500

    Refactor layout and navbar components

---
+++ src/components/image.tsx
@@ -0,0 +1,12 @@
+import NextImage, { ImageProps as DefaultImageProps, StaticImageData } from 'next/image';
+
+type ImageProps = DefaultImageProps & {
+  src: StaticImageData;
+  alt: string;
+};
+
+export const Img = (props: ImageProps) => {
+  const { src, alt, ...rest } = props;
+
+  return <NextImage src={src} alt={alt ?? ''} quality={100} {...rest} />;
+};
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import logo from 'public/logo.jpg';
+import logo from 'public/images/logo.png';
+import {Img} from './image';
 
 export const Navbar = () => {
     return (
         <nav className="nav">
             <div className="nav_brand">
-                <img src={logo} alt='logo' height={36} />
+                <Img src={logo} alt='logo' height={36} />
                 <h4 className="nav_title">Alex Grimes</h4>
                 <h4 className="nav_title xs">Alex G.</h4>
             </div>
