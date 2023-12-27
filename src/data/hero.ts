05945a8dcd81fa4cd5c9f6935cea5f4240a37333
alex <alexgrimes.se@gmail.com>
2023-12-27 16:50:43 -0500

    update text to be less over the top

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,7 +1,7 @@
 export const hero = {
     line1: `~ cd welcome`,
     line2: `~ cat welcome.txt`,
-    line3: `Greetings, fellow hackers and digital alchemists! This terminal houses my collection of code-forged artifacts, let's build something magnificent together.`,
+    line3: `Greetings, fellow hackers This terminal houses my collection, let's build something blazingly fast together.`,
     line6: ``,
     line7: ``,
 };
