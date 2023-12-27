3be00f590ba2ca34118fdafbf62b79d642e42021
alex <alexgrimes.se@gmail.com>
2023-12-27 17:03:38 -0500

    added to descriptions

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,7 +5,9 @@ export const experience: Experience[] = [
         _id: '04',
         year: '2022-Present',
         title: `Software Engineer`,
-        description: ``,
+        description: `Designed, developed, and maintained numerous systems and applications.
+        \nCollaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.
+        \n deployments and environments through well-defined CI/CD pipelines.`,
         company: `North Wind Group`,
         url: `https://northwindgrp.com/`,
     },
@@ -13,7 +15,9 @@ export const experience: Experience[] = [
         _id: '03',
         year: '2021-2022',
         title: `Software Engineer`,
-        description: ``,
+        description: `Developed, and maintained numerous systems and applications.
+        \nCollaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.
+        \n deployments and environments through well thought out SOP's and scripted deployments.`,
         company: `Michelin`,
         url: `https://www.michelinman.com/`,
     },
@@ -21,7 +25,9 @@ export const experience: Experience[] = [
         _id: '02',
         year: '2018-2021',
         title: `Application Administrator`,
-        description: ``,
+        description: `Adapted and customized existing applications to meet specific client needs, modifying source code.
+        \n Provided Level 2 technical support, resolving complex technical issues and diagnosing system performance across various platforms.
+        \n Collaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.`,
         company: `NWN Carousel`,
         url: `https://nwncarousel.com/`,
     },
