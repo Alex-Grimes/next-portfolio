74248e9261f9d8bd75b52c1525b35be1eb0a276a
alex <alexgrimes.se@gmail.com>
2024-06-18 16:44:34 -0400

    updated resume section with new role

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,34 +1,45 @@
-import { Experience } from '@/types';
+import { Experience } from "@/types";
 
 export const experience: Experience[] = [
-    {
-        _id: '04',
-        year: '2022-Present',
-        title: `Software Engineer`,
-        description: `Designed, developed, and maintained numerous systems and applications.
+  {
+    _id: "05",
+    year: "2024-Present",
+    title: `Software Engineer`,
+    description: `Worked as Software Engineer for multiple large teams developing software to help advanced a premier mortgage lender.
+    \nCollaborate with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.
+    \nBuilt and maintained a Large scale multitenant applications with users all across the country.`,
+    company: `Lima One Capital`,
+    url: `https://limaonecapital.com/`,
+  },
+
+  {
+    _id: "04",
+    year: "2022-2024",
+    title: `Software Engineer`,
+    description: `Designed, developed, and maintained numerous systems and applications.
         \nCollaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.
         \nEnhanced deployments and environments through well-defined CI/CD pipelines.`,
-        company: `North Wind Group`,
-        url: `https://northwindgrp.com/`,
-    },
-    {
-        _id: '03',
-        year: '2021-2022',
-        title: `Software Engineer`,
-        description: `Developed, and maintained numerous systems and applications.
+    company: `North Wind Group`,
+    url: `https://northwindgrp.com/`,
+  },
+  {
+    _id: "03",
+    year: "2021-2022",
+    title: `Software Engineer`,
+    description: `Developed, and maintained numerous systems and applications.
         \nCollaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.
         \nPerformed deployments and environments through well thought out SOP's and scripted deployments.`,
-        company: `Michelin`,
-        url: `https://www.michelinman.com/`,
-    },
-    {
-        _id: '02',
-        year: '2018-2021',
-        title: `Application Administrator`,
-        description: `Adapted and customized existing applications to meet specific client needs, modifying source code.
+    company: `Michelin`,
+    url: `https://www.michelinman.com/`,
+  },
+  {
+    _id: "02",
+    year: "2018-2021",
+    title: `Application Administrator`,
+    description: `Adapted and customized existing applications to meet specific client needs, modifying source code.
         \nProvided Level 2 technical support, resolving complex technical issues and diagnosing system performance across various platforms.
         \nCollaborated with cross-functional teams, including designers, project managers, stakeholders, and other developers, to deliver high-quality solutions.`,
-        company: `NWN Carousel`,
-        url: `https://nwncarousel.com/`,
-    },
+    company: `NWN Carousel`,
+    url: `https://nwncarousel.com/`,
+  },
 ];
